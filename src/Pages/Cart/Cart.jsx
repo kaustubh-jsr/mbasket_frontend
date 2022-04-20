92050e5b0e215ef8d8ceba0822d93e65188a8b38
Kaustubh P <kaustubhp.jsr@gmail.com>
2022-04-20 10:14:25 +0530

    Refactored cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -13,37 +13,46 @@ function Cart() {
       <div class="sidenav-section__div">
         <hr class="sidenav-section__divider" />
       </div>
-      <div class="cart-page-container grid-70-30">
-        <div class="cart-items">
-          {cart.state.cartItems.map((item) => (
-            <CartProductCard
-              item={item}
-              state={cart.state}
-              dispatch={cart.dispatch}
-            />
-          ))}
-        </div>
-        <div class="order-info-sidebar card">
-          <div class="satisfaction-grt grid-30-70">
-            <img src="https://i.postimg.cc/MTv3JmXd/ribbon.webp" alt="ribbon" />
-            <div class="grt-msg">
-              <h6 class="grt-msg-header">100 % satisfaction guarantee</h6>
-              <h6 class="grt-msg-text">Place your order with peace of mind.</h6>
+      {cart.state.cartItems.length === 0 ? (
+        <h3 className="text-center">Your cart is empty.</h3>
+      ) : (
+        <div class="cart-page-container grid-70-30">
+          <div class="cart-items">
+            {cart.state.cartItems.map((item) => (
+              <CartProductCard
+                item={item}
+                state={cart.state}
+                dispatch={cart.dispatch}
+              />
+            ))}
+          </div>
+          <div class="order-info-sidebar card">
+            <div class="satisfaction-grt grid-30-70">
+              <img
+                src="https://i.postimg.cc/MTv3JmXd/ribbon.webp"
+                alt="ribbon"
+              />
+              <div class="grt-msg">
+                <h6 class="grt-msg-header">100 % satisfaction guarantee</h6>
+                <h6 class="grt-msg-text">
+                  Place your order with peace of mind.
+                </h6>
+              </div>
             </div>
+            <div class="sidenav-section__div">
+              <hr class="sidenav-section__divider" />
+            </div>
+            <h5>
+              Subtotal <span class="h6">({cart.state.quantity} items)</span>:
+              Rs. {cart.state.total}
+            </h5>
+            <div class="sidenav-section__div">
+              <hr class="sidenav-section__divider" />
+            </div>
+            <button class="btn btn-success">Proceed to buy</button>
           </div>
-          <div class="sidenav-section__div">
-            <hr class="sidenav-section__divider" />
-          </div>
-          <h5>
-            Subtotal <span class="h6">({cart.state.quantity} items)</span>: Rs.{" "}
-            {cart.state.total}
-          </h5>
-          <div class="sidenav-section__div">
-            <hr class="sidenav-section__divider" />
-          </div>
-          <button class="btn btn-success">Proceed to buy</button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
